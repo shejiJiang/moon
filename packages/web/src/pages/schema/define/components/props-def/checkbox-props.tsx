a55963bbfecb84cbc407cd99bf992471a57b31b5
yangxiaodong <yangxiaodong@qianmi.com>
2019-07-01 11:31:19 +0800

    add: 添加类型进来..

--- packages/web/src/pages/schema/define/components/props-def/checkbox-props.tsx
+++ packages/web/src/pages/schema/define/components/props-def/checkbox-props.tsx
@@ -8,16 +8,21 @@
  **/
 
 import * as React from 'react';
-import { Input} from 'antd';
+import {Input} from 'antd';
+import {EInterActType} from "../../../../moon/page/typings";
 
 interface ICheckboxPropsP {
-  onChange: (param: {name: string; datas: {name: string; value: string}[]}) => void;
+  onChange: (
+    param: {
+      type:EInterActType,
+      name: string; datas: {name: string; value: string}[]},
+  ) => void;
   [name: string]: any;
 }
 
 interface ICheckboxPropsS {
   name: string;
-  value:string;
+  value: string;
   [name: string]: any;
 }
 
@@ -38,35 +43,39 @@ export default class CheckboxProps extends React.Component<
 
   render() {
     return (
-     <div>
-       <Input data-key={'name'} onChange={this._change} placeholder="变量名称" />
-       <Input
-         data-key={'values'}
-         onChange={this._change}
-         placeholder="format: key:value,key2:value or key1,key2"
-       />
-     </div>
+      <div>
+        <Input data-key={'name'} onChange={this._change} placeholder="变量名称" />
+        <Input
+          data-key={'values'}
+          onChange={this._change}
+          placeholder="format: key:value,key2:value or key1,key2"
+        />
+      </div>
     );
   }
 
   _change = e => {
-    this.setState({
-      [e.target.dataset.key]: e.target.value,
-    },this._notify);
+    this.setState(
+      {
+        [e.target.dataset.key]: e.target.value,
+      },
+      this._notify,
+    );
   };
 
-  _notify=()=>{
-    let  {values,name}=   this.state;
-    let datas = values.split(",").map(item=>{
-      let datas  = item.trim().split(":");
-      return  {
-        name:datas[0],
-        value:datas[1]||datas[0]
-      }
+  _notify = () => {
+    let {values, name} = this.state;
+    let datas = values.split(',').map(item => {
+      let datas = item.trim().split(':');
+      return {
+        name: datas[0],
+        value: datas[1] || datas[0],
+      };
     });
-
     this.props.onChange({
-      name,datas
+      type:EInterActType.checkboxChoose,
+      name,
+      datas,
     });
-  }
+  };
 }
