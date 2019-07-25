d60570f9bbda0e471b14eda7694531eb338141ef
yangxiaodong <yangxiaodong@qianmi.com>
2019-07-25 16:25:37 +0800

    生成页面核心添加测试用例;

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -10,13 +10,12 @@
 import * as ReduxGen  from  './page/redux/redux';
 import * as JsonUtil from  './util/json-util';
 import * as StringUtil from './util/string-util';
+import * as JestUtil from './util/jest-util';
 import * as CompileUtil from './util/compile-util';
 import * as WebApiGen from './web-api/client';
 import * as fakeGen from './web-api/client/fake-gen';
 import * as TsIndex from './web-api/client/ts-index';
 
-
-// export  ReduxGen;
 
 export default {
   ReduxGen,
@@ -24,6 +23,7 @@ export default {
   StringUtil,
   CompileUtil,
   fakeGen,
+  JestUtil,
   WebApiGen,
   TsIndex
 }
