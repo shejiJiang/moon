1f19e286edfb1b562b67d44eae1799cfa233535e
yangxiaodong <yangxiaodong@qianmi.com>
2019-07-25 18:39:25 +0800

    add : 添加 一个商标呢...

---
+++ packages/core/src/web-api/client/__tests__/api.test.ts
@@ -0,0 +1,16 @@
+/**
+ * @desc
+ *
+ * @使用场景
+ *
+ * @company qianmi.com
+ * @Date    2019/7/25
+ **/
+
+
+describe('api生成', () => {
+  it('简单api生成', async () => {
+
+    expect('hello').toEqual("hello");
+  });
+});
